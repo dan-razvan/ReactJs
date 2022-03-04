b2bccd5c128f33142731aba541e094d8ba3ffc39
dan-razvan <razvanbogdanturiatca@gmail.com>
2022-03-04 14:04:31 +0200

    destructured props

--- Button/src/Button.js
+++ Button/src/Button.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
 const Button = (props) => {
+  const { label } = props
   return (
     <button
       onClick={() => {
         console.log('Button')
       }}
     >
-      {props.label}
+      {label}
     </button>
   )
 }
