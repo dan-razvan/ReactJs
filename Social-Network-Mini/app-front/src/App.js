4097a602e81aaac873b0d65f91a9f5d7a7d45c41
dan-razvan <razvanbogdanturiatca@gmail.com>
2022-03-08 17:48:32 +0200

    added button functionality

--- Social-Network-Mini/app-front/src/App.js
+++ Social-Network-Mini/app-front/src/App.js
@@ -1,16 +1,34 @@
 import User from './components/user/User'
+import { useState } from 'react'
 
-const App = () => (
-  <div className="App">
-    <User
-      viewMode={'detailed'}
-      userData={{
-        avatar: '/images/avatar1.png',
-        nickname: 'John Doe',
-        lastOnline: '04.03.2022 18:00:00',
-      }}
-    />
-  </div>
-)
+const App = () => {
+  const [display, setDisplay] = useState('detailed')
+  const [button, setButton] = useState('show less')
+  const onClickHandler = () => {
+    if (display === 'compact') {
+      setDisplay('detailed')
+      setButton('show less')
+    }
+    if (display === 'detailed') {
+      setDisplay('compact')
+      setButton('show more')
+    }
+  }
+  return (
+    <div className="App">
+      <User
+        viewMode={display}
+        userData={{
+          avatar: '/images/avatar1.png',
+          nickname: 'John Doe',
+          lastOnline: '04.03.2022 18:00:00',
+        }}
+      />
+      <button className="button" onClick={onClickHandler}>
+        {button}
+      </button>
+    </div>
+  )
+}
 
 export default App
--- Social-Network-Mini/app-front/src/components/user/User.js
+++ Social-Network-Mini/app-front/src/components/user/User.js
@@ -1,25 +1,45 @@
-// Main User Module
-// FUNCTIONAL COMPONENT
+import { useEffect, useState } from 'react'
 
-// User Component:
-// * user data
-// * view modes: compact, detailed
+// constructor
 const User = ({ userData: { avatar, nickname, lastOnline }, viewMode }) => {
-  if (viewMode === 'detailed') {
-    return (
-      <div className="User">
-        <img src={avatar} />
-        <h2>{nickname}</h2>
-        <p>{lastOnline}</p>
-      </div>
-    )
-  } else if (viewMode === 'compact') {
-    return (
-      <div className="User">
-        <img src={avatar} width="80px" />
-      </div>
-    )
-  }
+  const [online, setOnline] = useState()
+
+  useEffect(() => {
+    // console.info('User component did mount or update')
+    // setTimeout(() => {
+    //   setOnline(!online)
+    //   console.info(`The user is ${online ? 'online' : 'offline'}`)
+    // }, 2000)
+    if (viewMode === 'compact') {
+      setOnline(false)
+    }
+    if (viewMode === 'detailed') {
+      setOnline(true)
+    }
+  }, [viewMode])
+
+  return (
+    <div className="User">
+      {viewMode === 'compact' ? (
+        <div>
+          <img src={avatar} alt="avatar" style={{ width: '80px' }} />
+        </div>
+      ) : (
+        <div>
+          <img src={avatar} alt="avatar" />
+          <h2>{nickname}</h2>
+          <p>{lastOnline}</p>
+        </div>
+      )}
+
+      {/* DOM/JSX fragment depends on this variable*/}
+      {online ? (
+        <span style={{ color: 'green', fontSize: '40px' }}>◉</span>
+      ) : (
+        <span style={{ color: 'gray', fontSize: '40px' }}>◉</span>
+      )}
+    </div>
+  )
 }
 
 export default User
